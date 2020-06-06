2cecd621c84b0a6be702476efd3179acd7823fbe
Nils Knappmeier <github@knappi.org>
2020-06-06 23:11:11 +0200

    add files for working poc

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # docker-image-selenium-sshd
+
+
+ ./chisel_1.5.2_linux_amd64 client localhost:2222 R:8000:localhost:8000
--- test/tunnel.test.js
+++ test/tunnel.test.js
@@ -1,17 +1,30 @@
-const reverseTunnel = require('./lib/reverse-tunnel')
 const webdriver = require('selenium-webdriver')
 const fs = require("fs-extra")
+const express = require('express')
+const http = require('http')
+
+const app = express();
+app.use(express.static('test/public'));
 
 describe("the tunnel", () => {
-
-  let tunnel
-
-  beforeAll(async () => {
-    tunnel = await reverseTunnel.createTunnel();
-  },100000)
+  let server;
+  beforeAll(async() => {
+    server = http.createServer(app).listen(8000)
+    return new Promise(((resolve, reject) => {
+      server.on('listening', () => resolve())
+      server.on('error', err => reject(err))
+    }))
+  })
 
   afterAll(async () => {
-    await reverseTunnel.closeTunnel(tunnel);
+    return new Promise((resolve, reject) => {
+      server.close(err => {
+        if (err != null) {
+          return reject(err)
+        }
+        return resolve()
+      })
+    })
   })
 
   it("should work", async () => {
@@ -20,7 +33,7 @@ describe("the tunnel", () => {
         .usingServer('http://localhost:4444/wd/hub')
         .build();
     try {
-      await driver.get('http://localhost:8000')
+      await driver.get('http://localhost:8000/test.html')
       const screenshot = await driver.takeScreenshot();
       await fs.writeFile('out.png', screenshot, 'base64')
     } finally {
